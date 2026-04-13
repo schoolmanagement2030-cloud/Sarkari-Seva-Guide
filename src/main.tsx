1b185f1495e3f0027565bed8d1c3c7218543388f
schoolmanagement2030-cloud <schoolmanagement2030@gmail.com>
2026-04-13 11:59:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// 🔥 IMPORTANT
+import { HashRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 );
